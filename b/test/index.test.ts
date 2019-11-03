110887c0cdc82abba2034061c7cb6d9dc4736386
Matt Phillips <matt@mattphillips.io>
2019-11-03 12:01:45 +0000

    Add workspace subproject dependency

--- b/test/index.test.ts
+++ b/test/index.test.ts
@@ -1,7 +1,12 @@
+import { Foo } from 'a';
 import { Bar } from 'src/Bar';
 
 describe('Bar', () => {
   it('should be Bar', () => {
     expect(Bar).toBe('Bar');
+  });
+
+  it('should be FooBar', () => {
+    expect(Foo + Bar).toBe('FooBar');
   });
 });
